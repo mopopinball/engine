6677e3b434e0dca682f9ec42e52d004df58133f3
Sebastian Gsellmann <sirvannos@hotmail.com>
2021-05-09 20:33:52 -0400

    fix: switch to more simple logger

--- src/system/logger.ts
+++ src/system/logger.ts
@@ -1,27 +1,28 @@
-import {createLogger, format, transports} from 'winston';
+// import {createLogger, format, transports} from 'winston';
+// import Debug from 'debug';
+import log from 'loglevel';
 
-/**
- * Logging.
- */
-export const logger = createLogger({
-    level: 'debug',
-    format: format.combine(
-        format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss.SSS'
-        }),
-        format.simple()
-    ),
-    transports: [],
-});
+export const logger = log;
 
-if (process.env.NODE_ENV === 'production') {
-    logger.add(new transports.File({
-        filename: '/home/pi/mopo/engine/mopo.log',
-        maxsize: 4194304,
-        maxFiles: 5,
-        tailable: true
-    }));
-}
-else {
-    logger.add(new transports.Console());
-}
+// export const logger = createLogger({
+//     level: 'debug',
+//     format: format.combine(
+//         format.timestamp({
+//             format: 'YYYY-MM-DD HH:mm:ss.SSS'
+//         }),
+//         format.simple()
+//     ),
+//     transports: [],
+// });
+
+// if (process.env.NODE_ENV === 'production') {
+//     logger.add(new transports.File({
+//         filename: '/home/pi/mopo/engine/mopo.log',
+//         maxsize: 4194304,
+//         maxFiles: 5,
+//         tailable: true
+//     }));
+// }
+// else {
+//     logger.add(new transports.Console());
+// }
