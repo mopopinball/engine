5753a7c13af45696f3bf20daf0e028a639ab8e9e
Sebastian Gsellmann <sirvannos@hotmail.com>
2024-05-18 13:59:01 -0400

    fix: export updates

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 export * from './src/system/rule-engine/rule-engine';
 export * from './src/system/hardware-config.schema';
 export * from './src/system/devices/lamp-role';
+
+export {default as panthera} from './src/games/panthera/hardware-config.json';
