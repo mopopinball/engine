9390febcbb138f2efbdc4ebd00c70f094da1a1a0
Sebastian Gsellmann <sirvannos@hotmail.com>
2024-07-27 14:48:24 +0000

    refactor: add playfield lamp to export

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ export * from './src/system/hardware-config.schema';
 export * from './src/system/devices/lamp-role';
 export * from './src/system/devices/output-device-type';
 export * from './src/system/devices/light';
+export * from './src/system/devices/playfield-lamp';
 export * from './src/system/rule-engine/desired-output-state';
 
 export {default as panthera} from './src/games/panthera/hardware-config.json';
