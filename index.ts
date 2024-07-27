80cb0d8ba9f7265232aa9ed49108845f3e75e8dd
Sebastian Gsellmann <sirvannos@hotmail.com>
2024-07-27 14:51:04 +0000

    fix: add playfield switch to export

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ export * from './src/system/devices/lamp-role';
 export * from './src/system/devices/output-device-type';
 export * from './src/system/devices/light';
 export * from './src/system/devices/playfield-lamp';
+export * from './src/system/devices/playfield-switch';
 export * from './src/system/rule-engine/desired-output-state';
 
 export {default as panthera} from './src/games/panthera/hardware-config.json';
